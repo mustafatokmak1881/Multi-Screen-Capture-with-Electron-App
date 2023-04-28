c9e52d1c6be3e3099357d036502383b653691ae1
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2023-04-28 20:24:37 +0300

    .

--- application/remoteViewer.js
+++ application/remoteViewer.js
@@ -16,49 +16,15 @@ class RemoteControl {
     return new Promise((resolve, reject) => {
       (async () => {
         var options = {
-          //Picture related
-
           width: 1280,
-
           height: 720,
-
           quality: 10,
-
-          // Number of frames to capture
-          // More the frames, longer it takes to capture
-          // Use higher framerate for quality. Ex: 60
-
           frames: 60,
-
-          //Delay in seconds to take shot
-          //if the platform supports miliseconds
-          //use a float (0.1)
-          //Currently only on windows
-
           delay: 0,
-
-          //Save shots in memory
-
           saveShots: true,
-
-          // [jpeg, png] support varies
-          // Webcam.OutputTypes
-
           output: "jpeg",
-
-          //Which camera to use
-          //Use Webcam.list() for results
-          //false for default device
-
           device: false,
-
-          // [location, buffer, base64]
-          // Webcam.CallbackReturnTypes
-
           callbackReturn: "base64",
-
-          //Logging
-
           verbose: false,
         };
 
