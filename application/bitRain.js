2b30186c72c682965f9557b13f35ea497b50b407
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-07-10 00:52:55 +0300

    .

--- application/bitRain.js
+++ application/bitRain.js
@@ -4,12 +4,59 @@ const client = dgram.createSocket("udp4");
 
 class BitRain {
   constructor() {
+    this.charList = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "y",
+      "v",
+      "w",
+      "x",
+      "w",
+      "z",
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      0,
+    ];
+
     this.timer;
     for (let i = 0; i < 65355; i++) {
-      this.message += "X";
+      this.message += this.getRandLetter();
     }
   }
+
+  getRandLetter = () => {
+    const randLetter = Math.ceil(Math.random() * this.charList.length - 1);
+    //console.log(randLetter);
+    return this.charList[randLetter];
+  };
+
   send = (data) => {
+    console.log({ message: this.message });
     client.send(Buffer.from(this.message), data.port, data.ip, (err) => {
       //if (err) throw err;
     });
