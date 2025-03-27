3fdad07e8d4c3e3605611878360e3ab544bcd875
Beyn <mehmet.tokmak@beyn.com.tr>
2025-03-27 20:23:51 +0300

    .

--- updater/index.js
+++ updater/index.js
@@ -2,7 +2,48 @@ const request = require('request');
 const dummy = require('./dummy')
 
 
-getMe = (id) => {
+
+getList = (count) => {
+    return new Promise((resolve, reject) => {
+        (async () => {
+            var options = {
+                'method': 'POST',
+                'url': 'https://bo-api.xpress-ix.com/sysapi/v1/terminal/list',
+                'headers': {
+                    'accept': 'application/json, text/plain, */*',
+                    'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'origin': 'https://clientoffice.xpress-ix.com',
+                    'priority': 'u=1, i',
+                    'referer': 'https://clientoffice.xpress-ix.com/',
+                    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'same-site',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+                },
+                form: {
+                    'length': count,
+                    'page': '1',
+                    'status': '1',
+                    'site_id': '9359',
+                    'boToken': 'f7701b926a68d20a5a91ee29c4052105',
+                    'external_login': 'false'
+                }
+            };
+            request(options, function (error, response) {
+                if (error) throw new Error(error);
+                const result = (JSON.parse(response.body)).data.terminals.map(item => item.id)
+                resolve(result);
+            });
+
+        })()
+    });
+}
+
+sendCmd = (id) => {
     return new Promise((resolve, reject) => {
         (async () => {
             var options = {
@@ -38,25 +79,19 @@ getMe = (id) => {
     });
 }
 
-// let counter = 16380;
-// (async () => {
-//     setInterval(async () => {
-//         const result = await getMe(counter);
-//         console.log({ result, counter });
-//         counter--;
-//     }, 5000);
-// })()
 
 
 
-console.log({ dummy });
 
 
 let counter = 0;
 setInterval(async () => {
-    if (dummy[counter]) {
-        const result = await getMe(dummy[counter]);
-        console.log({ result, counter: dummy[counter] });
+    const list = await getList(3000);
+    console.log({ count: list.length });
+
+    if (list[counter]) {
+        const result = await sendCmd(list[counter]);
+        console.log({ counter: list[counter], result });
         counter++;
     }
-}, 5000)
+}, 3000)
